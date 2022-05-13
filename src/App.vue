c23f7acf9c828819404d5f248de00b01490b87bf
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-05-13 15:45:07 +0200

    first steps header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
   <div id="app">
-    
+    <AppHeader/>
   </div>
 </template>
 
 <script>
+import AppHeader from "./components/AppHeader.vue";
 
 
 export default {
-  
+    components: { AppHeader }
 }
 </script>
 
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,79 @@
+<template>
+  <header>
+      <nav>
+        <div class="logo">
+            <img src="../assets/img/dc-logo.png" alt="">
+        </div>
+        <ul>
+            <li v-for="(link, index)  in links" :key="`nav-${index}`">
+                <a :href="link.href">{{link.text}}</a>
+            </li>
+        </ul>
+      </nav>
+  </header>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            links:[
+                {
+                    text : "characters",
+                    active : false,
+                    href : "#"
+                },
+                {
+                    text : "comics",
+                    active : false,
+                    href : "#"
+                },
+                {
+                    text : "movies",
+                    active : false,
+                    href : "#"
+                },
+                {
+                    text : "tv",
+                    active : false,
+                    href : "#"
+                },
+                {
+                    text : "games",
+                    active : false,
+                    href : "#"
+                },
+                {
+                    text : "collectibles",
+                    active : false,
+                    href : "#"
+                },
+                {
+                    text : "videos",
+                    active : false,
+                    href : "#"
+                },
+                {
+                    text : "fans",
+                    active : false,
+                    href : "#"
+                },
+                {
+                    text : "news",
+                    active : false,
+                    href : "#"
+                },
+                {
+                    text : "shop",
+                    active : false,
+                    href : "#"
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
