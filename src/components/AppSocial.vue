42ac3c3d30b69b7d6a6a15ae0dd91551020ea4d3
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-05-13 18:08:20 +0200

    socials

--- src/components/AppSocial.vue
+++ src/components/AppSocial.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="socialsBar">
-      <div class="signup">
+     <div class="container">
+        <div class="signup">
         <a class="sign tbn">sign-up now!</a>
       </div>
       <div class="socials">
         <span>follow us</span>
         <img src="../assets/img/footer-facebook.png" alt="">
+        <img src="../assets/img/footer-periscope.png" alt="">
+        <img src="../assets/img/footer-pinterest.png" alt="">
+        <img src="../assets/img/footer-twitter.png" alt="">
+        <img src="../assets/img/footer-youtube.png" alt="">
       </div>
+     </div>
   </div>
 </template>
 
@@ -19,4 +25,5 @@ export default {
 <style lang="scss" scoped>
 @import "../assets/styles/_global.scss";
 @import "../assets/styles/_mixin.scss";
+@import "../assets/styles/_social.scss";
 </style>
